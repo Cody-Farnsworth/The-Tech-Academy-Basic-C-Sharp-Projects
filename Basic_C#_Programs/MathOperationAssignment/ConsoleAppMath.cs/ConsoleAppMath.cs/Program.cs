e29017925621266ce7660a18ed4acc4c1b614f4b
Cody-Farnsworth <86331200+Cody-Farnsworth@users.noreply.github.com>
2022-02-04 13:35:08 -0800

    Sub

---
+++ Basic_C#_Programs/MathOperationAssignment/ConsoleAppMath.cs/ConsoleAppMath.cs/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleAppMath.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please Choose a Number");
+            string UserNumS = Console.ReadLine();
+            int UserNum = Convert.ToInt32(UserNumS);
+            int  TotalM= UserNum * 50;          
+            Console.WriteLine(" Your number multiplied by 50 is: " + TotalM);
+            Console.WriteLine("Please Choose another Number");
+            string UserNumS2 = Console.ReadLine();
+            int UserNum2 = Convert.ToInt32(UserNumS2);
+            int TotalA = UserNum2 + 25;
+            Console.WriteLine(" Your number plus 25 is: " + TotalA);
+            Console.WriteLine("Please Choose another Number");
+            string UserNumS3 = Console.ReadLine();
+            int UserNum3 = Convert.ToInt32(UserNumS3);
+            float TotalD = (float)(UserNum3 / 12.5f);
+            Console.WriteLine(" Your number divided by 12.5 is: " + TotalD);
+            Console.WriteLine("Please Choose another Number");
+            string UserNumS4 = Console.ReadLine();
+            int UserNum4 = Convert.ToInt32(UserNumS4);
+            bool TotalG = UserNum4 > 50;
+            Console.WriteLine("Is your number greater then 50? " + TotalG);
+            Console.WriteLine("Please Choose a Number");
+            string UserNumS5 = Console.ReadLine();
+            int UserNum5 = Convert.ToInt32(UserNumS5);
+            int TotalL = UserNum5 % 7;
+            Console.WriteLine("The remainder of you number divided by 7 is: " + TotalL);
+
+            Console.Read();
+
+
+        }
+    }
+}
