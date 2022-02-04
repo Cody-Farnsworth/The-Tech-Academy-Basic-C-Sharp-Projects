73580cc53c88b3213c65393bcb10c69cbeb0eca9
Cody-Farnsworth <86331200+Cody-Farnsworth@users.noreply.github.com>
2022-02-04 12:48:06 -0800

    Sub

---
+++ Basic_C#_Programs/DailyReportAssignment/DailyReportAss/DailyReportAss.cs/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DailyReportAss.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy \nStudent Daily Report");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            string course = Console.ReadLine();
+            Console.WriteLine("What Page Number?");
+            string Page = Console.ReadLine();
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false.\"");
+            string TorF = Console.ReadLine();
+            Console.WriteLine("Were there any positive experiences you'd like to share?Please give specifics.");
+            string experiences = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you's like to provide?Please be specific.");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string studyHrs = Console.ReadLine();
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this Shortly. \nHave a great day!");
+            Console.Read();
+
+            // This very simple code will display somne messages for the student to answer. After each answer is given it will store them in each variable name for that answer.
+            //Then it will give them a nice goodbye message and end the program.
+
+
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/VarandData/VariabledAndDataTypes/VariabledAndDataTypes.cs/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace VariabledAndDataTypes.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this is a simple program
+            //console.writeline("what is youre name?");
+            //string yourname = console.readline();
+            //console.writeline("your name is: " + yourname);
+            //console.readline();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            //bool isStuding = false;
+            //byte hoursWorked = 42;
+            //SByte currentTemp = -23;
+            //char questionMark = '\u2103';
+
+            //decimal moneyInBank = 100.5m;
+
+            //double heightInCentimeters = 211.30202092;
+
+            //float secondsLeft = 2.62F;
+
+            //short tempOnMars = -341;
+
+            //string myName = "Cody";
+
+            //int currantAge = 30;
+            //string yeatsOld = currantAge.ToString();
+
+            //bool isRaing = true;
+           // string raingingStatus = Convert.ToString(isRaing);
+
+            //Console.WriteLine(raingingStatus);
+           // Console.ReadLine();
+        }
+    }
+}
