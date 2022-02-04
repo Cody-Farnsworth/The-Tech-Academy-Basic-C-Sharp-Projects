8a1b56a4c55b9507ce2ab620cf0740877de05731
Cody-Farnsworth <86331200+Cody-Farnsworth@users.noreply.github.com>
2022-02-04 14:31:36 -0800

    Sub

---
+++ Basic_C#_Programs/BooleanLogicAssignment/BooleanLogic/BooleanLogic.cs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BooleanLogic.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            int ageI = Convert.ToInt32(age);
+            Console.WriteLine("Have you ever had a DUI? True or False");
+            string DUI = Console.ReadLine();
+            bool DUIB = Convert.ToBoolean(DUI);
+            Console.WriteLine("How many speeding tickets do you have?");
+            string Speed = Console.ReadLine();
+            int SpeedI = Convert.ToInt32(Speed);
+            bool ageB = ageI > 15;
+            bool DUIBB = DUIB == false;
+            bool SpeedB = SpeedI < 3;
+            bool Qualified = ageB && DUIBB && SpeedB;
+            Console.WriteLine("Qualified? " + Qualified);
+        }
+    }
+}
