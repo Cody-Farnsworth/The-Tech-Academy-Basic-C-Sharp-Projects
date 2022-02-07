f1c8411d0986c00af0784cbef4274f9d7ae0a64e
Cody-Farnsworth <86331200+Cody-Farnsworth@users.noreply.github.com>
2022-02-07 12:17:52 -0800

    fixs

--- Basic_C#_Programs/VarandData/VariabledAndDataTypes/VariabledAndDataTypes.cs/Program.cs
+++ Basic_C#_Programs/VarandData/VariabledAndDataTypes/VariabledAndDataTypes.cs/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace VariabledAndDataTypes.cs
 {
@@ -10,7 +14,7 @@ namespace VariabledAndDataTypes.cs
             // This Section is for the diffrent Data types!!!!!
 
 
-
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
             // this is a simple program
             //console.writeline("what is youre name?");
             //string yourname = console.readline();
@@ -50,7 +54,7 @@ namespace VariabledAndDataTypes.cs
 
 
 
-
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
             // This section is for Math Operations.
 
@@ -73,11 +77,10 @@ namespace VariabledAndDataTypes.cs
             // you can put "double" insted of int and make the numbers decimals and it will give you a non rounded number.
 
 
-
             //remanders section
             //int remainder = 10 % 2;even number will return 0 if the 10 was 11 it will return a 1 or odd.
 
-
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
             //Comparision section
             //bool TrueOrFalse = 12 > 5; This returns a true
             //bool TrueOrFalse = 12 < 5; This returns a false
@@ -112,7 +115,7 @@ namespace VariabledAndDataTypes.cs
             //true ^ true = false
             //false ^ false = false
 
-
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
             // If Else section
             //int currentTemp = 70;
             //int rommTemp = 70;
@@ -128,8 +131,68 @@ namespace VariabledAndDataTypes.cs
             //}
             //Console.ReadLine();
 
+            //short hand turnary operator
+            //string Results = var == var ? "" : "";
 
 
+            //userimput example
+            //int "" = Convert.ToInt32(Console.ReadLine());
+
+
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            //switch statments
+
+            //int day = 4;
+            //switch (day)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Monday");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Tuesday");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Wednesday");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Thrsday");
+            //        break;
+
+            //        Outputs "Thursday" (day 4)
+
+            // default: is the final switch for everything that wasnting in the switch.
+            //}
+
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            //While loop
+
+            //while (!"") then put the switch inside the brackets
+            //make sure the write lines and variables are inside each case in order to keep the while loop going.
+            // put a DO statement before the while loop in order to get it to start the loop at least once
+
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+            // Sting helpful tips
+
+            // \ what follows the backslash is part of the string, this lets "" to be entered in the string for a \ in the string you do a \\
+            // \n is for a new line in the same statement
+            // \t is for a tab
+            // the @ tells the program that everything inside the "" is part of the string
+
+            // string name = "Jesse";
+            // bool TorF = name.Contains("s"); True
+            // TorF = name.EndsWith("s"): False
+
+            //int length = name.Length; 5
+            //.ToUpper() changes it to all upper case
+            //.ToLower() changes to all lower case
+
+            //StringBuilder sb = new StringBuilder();
+
+            //sb.Append("My name is Jesse");
         }
     }
 }
