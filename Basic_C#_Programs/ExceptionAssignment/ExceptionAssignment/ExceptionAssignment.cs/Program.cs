6cdebc3adc866e274f9313fc59cf918bfa1455dc
Cody-Farnsworth <86331200+Cody-Farnsworth@users.noreply.github.com>
2022-02-08 13:14:48 -0800

    sub

---
+++ Basic_C#_Programs/ExceptionAssignment/ExceptionAssignment/ExceptionAssignment.cs/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionAssignment.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                List<int> Numbers = new List<int> { 10, 20, 30, 40, 50 };
+                Console.WriteLine("Please give me any number.");
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+                foreach (int number in Numbers)
+                {
+                    int total = number / userNumber;
+                    Console.WriteLine(total);
+                }
+            }
+            //catch(FormatException ex)
+            //{
+            //    Console.Write(ex.Message);
+            //}
+            catch(DivideByZeroException ex)
+            {
+                Console.Write(ex.Message);               
+            }
+            catch(Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+            finally
+            {
+                Console.Write("\nThe end");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
