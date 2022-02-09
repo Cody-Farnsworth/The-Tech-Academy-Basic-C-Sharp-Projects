420966e8721808489f09304b87780ec8df578fa2
Cody-Farnsworth <86331200+Cody-Farnsworth@users.noreply.github.com>
2022-02-09 15:15:53 -0800

    Sub

---
+++ Basic_C#_Programs/MethodAssignment/MathodsAssignment4/MethodAssignment4/MethodAssignment4.cs/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodAssignment4.cs
+{
+    public class Class1
+    {
+        public void add(int num1, int num2)
+        {
+            int res = 6 + num1;
+            Console.WriteLine("The second number is {0}", num2);           
+        }
+    }   
+}
---
+++ Basic_C#_Programs/MethodAssignment/MathodsAssignment4/MethodAssignment4/MethodAssignment4.cs/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodAssignment4.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 math = new Class1();
+            math.add(2, 3);
+            math.add(num1: 5, num2: 6);           
+        }
+    }
+}
