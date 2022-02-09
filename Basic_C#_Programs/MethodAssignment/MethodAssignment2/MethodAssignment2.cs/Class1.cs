66b7202dd104d1387c3ae9a84ddff6c02a317317
Cody-Farnsworth <86331200+Cody-Farnsworth@users.noreply.github.com>
2022-02-09 13:25:17 -0800

    sub

---
+++ Basic_C#_Programs/MethodAssignment/MethodAssignment2/MethodAssignment2.cs/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodAssignment2.cs
+{
+    public class Class1
+    {
+        public int add(int number)
+        {
+            return number + 10;
+        }
+        public int add(decimal number2)
+        {
+            return Convert.ToInt32(number2 * 6.5m);
+        }
+        public int add(string number3)
+        {
+            int number4 = Convert.ToInt32(number3);
+            return number4 - 2;
+
+        }
+    }
+    
+}
---
+++ Basic_C#_Programs/MethodAssignment/MethodAssignment2/MethodAssignment2.cs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MethodAssignment2.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 math = new Class1();
+            int number = 10;
+            decimal number2 = 6.5m;
+            string number3 = "10";            
+            Console.WriteLine(math.add(number));
+            Console.WriteLine(math.add(number2));
+            Console.WriteLine(math.add(number3));
+            Console.ReadLine();
+        }       
+    }
+}
