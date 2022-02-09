ceaa18973e51e62bcc014fef2cfc879e412b41de
Cody-Farnsworth <86331200+Cody-Farnsworth@users.noreply.github.com>
2022-02-09 14:12:37 -0800

    Sub

---
+++ Basic_C#_Programs/MethodAssignment/MethodAssignment3/MethodAssignment3.cs/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodAssignment3.cs
+{
+    public class Class1
+    {
+        public int add(int NumberOne, String NumberTwo)
+        {
+            if (NumberTwo == "")
+            {
+                return NumberOne; 
+            }
+            else
+            {
+                int Number2 = Convert.ToInt32(NumberTwo);
+                return NumberOne + Number2;
+            }
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodAssignment/MethodAssignment3/MethodAssignment3.cs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MethodAssignment3.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 math = new Class1();
+            Console.WriteLine("Please give me a number");
+            int NumberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You can give me another number if you would like if not leave blanke.");
+            string NumberTwo = Console.ReadLine();
+            Console.WriteLine(math.add(NumberOne, NumberTwo));
+            Console.ReadLine();
+        }
+    }
+}
