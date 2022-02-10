a2239982cdf8bc5f4efe8935e937d880be7dc10e
Cody-Farnsworth <86331200+Cody-Farnsworth@users.noreply.github.com>
2022-02-10 15:19:17 -0800

    Sub

---
+++ Basic_C#_Programs/MethodAssignment/MathodsAssignment5/MethodAssignment5/MethodAssignment5.cs/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MethodAssignment5.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
