ebac4b22c457e04eee05d4f4d70a7f0c18be2fa9
Cody-Farnsworth <86331200+Cody-Farnsworth@users.noreply.github.com>
2022-02-10 14:42:17 -0800

    sub

---
+++ Basic_C#_Programs/ClassAssignment/ConsoleAssignment/ClassAssignment.cs/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassAssignment.cs
+{
+
+    static class Numbers
+    {
+        public static int Number1 = 1;
+
+        public static void show()
+        {
+            Console.WriteLine("This is a class static example: ");
+        }
+    }
+
+
+    public class Class1
+    {
+        public void divid(int num1)
+        {
+            int sol = num1 / 2;
+            Console.WriteLine("Your number divided by 2 is: {0}", sol);
+        }
+        public void divid(int num1, int num2)
+        {
+            int sol2 = num1 / num2 / 2;
+            Console.WriteLine("Your first number divided by your second number is: {0}", sol2);
+        }
+    }
+}
+
+
---
+++ Basic_C#_Programs/ClassAssignment/ConsoleAssignment/ClassAssignment.cs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassAssignment.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 math = new Class1();
+            int initializeInMethod;
+            Method(out initializeInMethod);
+            Console.WriteLine("Please give me any number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please give me another number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            math.divid(num1);
+            math.divid(num1, num2);
+            Console.WriteLine("An example of using an out: {0} ", initializeInMethod);
+            Numbers.show();
+            Console.WriteLine("The number one is: {0}", Numbers.Number1);
+            Console.ReadLine();
+        }
+
+        static void Method(out int number)
+        {
+            number = 44;
+        }
+    }
+}
