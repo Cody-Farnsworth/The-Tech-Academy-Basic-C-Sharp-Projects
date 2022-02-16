0673b29a186d91d6cb4fe03a17f4f54bdf6165cf
Cody-Farnsworth <86331200+Cody-Farnsworth@users.noreply.github.com>
2022-02-16 14:21:51 -0800

    Sub

---
+++ Basic_C#_Programs/PolyAssignment/PolyAssignment2/PolyAssignment2.cs/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolyAssignment2.cs
+{
+    public class Employee : Person, IQuittable
+    {
+        public int Id { get; set; }
+        public void Quit()
+        {
+            Console.WriteLine("They Quit");
+        }
+    }    
+}
---
+++ Basic_C#_Programs/PolyAssignment/PolyAssignment2/PolyAssignment2.cs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PolyAssignment2.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            IQuittable quiter = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.SayName();
+            quiter.Quit();
+            Console.ReadLine();
+        }
+    }
+}
