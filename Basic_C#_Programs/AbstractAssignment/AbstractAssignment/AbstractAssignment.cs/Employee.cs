7a1fdde8676bf7f1786b3223f5c6d6b218be54c7
Cody-Farnsworth <86331200+Cody-Farnsworth@users.noreply.github.com>
2022-02-16 13:14:11 -0800

    Sub

---
+++ Basic_C#_Programs/AbstractAssignment/AbstractAssignment/AbstractAssignment.cs/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractAssignment.cs
+{
+    public class Employee: Person
+    {
+        public override void SayName()
+        {
+            base.SayName();
+        }
+    }
+}
---
+++ Basic_C#_Programs/AbstractAssignment/AbstractAssignment/AbstractAssignment.cs/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AbstractAssignment.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
